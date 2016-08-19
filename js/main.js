f310c70e415f529ee8658dc2a9c5f276ce83a6d1
Anett_Soos <anett.soos@city.ac.uk>
2016-08-19 11:26:36 +0100

    string templates

--- js/main.js
+++ js/main.js
@@ -13,13 +13,19 @@ var deliveryBoy = {
 }
 deliveryBoy.receive();
 
-// spread operator
-let first = [1, 2, 3];
+// string templates
 
-function addThreeThings(a,b,c){
-	let result = a + b + c;
-	console.log(result);
+var x = 1;
+var y = 2;
+var equation = `${x} + ${y} = ${x+y}` 
+console.log(equation);  //it will print 1 + 2 = 3
+
+function tag(strings, ...values){
+	if(values[0] <20) {
+		values[1] = "awake";
+	}
+	return `${strings[0]}${values[0]}${strings[1]}${values[1]}`
 }
 
-addThreeThings (... first) // prints 6 
-						   // '...' is spreading the array out to its individual elements
+var message = tag`It's ${new Date().getHours()} I'm ${""}`;
+console.log(message); //it will print "I'm awake" before 8pm
