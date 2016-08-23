6b83c1b35c8f7b1b15a11ab8439ba1b2288a2f53
Anett_Soos <anett.soos@city.ac.uk>
2016-08-23 11:33:59 +0100

    js for loop

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,35 @@
 // ES6 tutorial
-//Maps
+//for method
 
-var myMap = new Map();
+function getStockSymbols(stocks) {
+	var symbols = [],
+		counter,
+		stock
 
-//API // set() get() size clear() has()
+	for(counter = 0; counter < stocks.length; counter++) {
+		stock = stocks[counter];
+		symbols.push(stock.symbol);
+	}
 
-myMap.set('foo', 'bar'); //setting some key-value pairs on the new map
-myMap.set('hello', 'world');
-console.log(myMap.get('foo')); //will print 'bar' as it is the value associated with 'foo' key
-console.log(myMap.size); // prints 2
-myMap.clear();  
-console.log(myMap.size); // prints 0
-
-var myMap_two = new Map();
-myMap_two.set('foo', 'bar');
-myMap_two.set('hello', 'world');
-
-console.log(myMap_two.has('foo'));  //prints 'true'
-console.log(myMap_two.has('asdfsadf')); //prints 'false'
-
-//Iterators // keys() entries() values
-
-for (var key of myMap_two.keys()){  //prints "foo" and "hello"
-	console.log(key);
+	return symbols;
 }
 
-for (var value of myMap_two.values()){ //prints "bar" and "hello"
-	console.log(value);
-}
+var symbols = getStockSymbols([
+	{
+		symbol: "XGC",
+		price: 240.00,
+		colume: 2342
+	},
+	{
+		symbol: "AAD",
+		price: 30.00,
+		colume: 12
+	},
+	{
+		symbol: "TDF",
+		price: 140.20,
+		colume: 1001
+	}
+]);
 
-for (var[key, value] of myMap_two.entries()){ //foo = bar hello = world
-	console.log(key + ' = ' + value);
-}
+console.log(JSON.stringify(symbols));
