d56567914e74740cc31e03f4c7a1cbe1843db819
Anett_Soos <anett.soos@city.ac.uk>
2016-08-23 14:02:13 +0100

    js filter method

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,53 @@
 // ES6 tutorial
 //forEach => this can run asynchronously
 
-function getStockSymbols(stocks) {
-	return stocks.map(function(stock) {
-		return stock.symbol;
-	})
-}
-
-Array.prototype.map = function(projection) { //explanation
+/*function getStockOver(stocks, minPrice) {
 	var results = [];
 
-	this.forEach(function(item){
-		results.push(projection(item));
+	stocks.forEach(function(stock) {
+		if (stock.price >= minPrice) {
+			results.push(stock);
+		} 
+	});
+	return results;
+}*/
+
+function getStockOver(stocks, minPrice) { //has the same result as above function
+	return stocks.filter(function(stock){
+		return stock.price >= minPrice;
+	});
+}
+
+Array.prototype.filter = function(predicate) { //the filter method accepts a function called a predicate
+	var results = [];
+
+	this.forEach(function(item) { //'this' is the Array because 'this' filters a method on Array
+		if (predicate(item)) { //apply the predicate() to the item,if it returns true, it will be added to results
+			results.push(item);
+		}
 	});
 	return results;
 };
 
-var symbols = getStockSymbols([
-	{
-		symbol: "XGC",
-		price: 240.00,
-		colume: 2342
-	},
-	{
-		symbol: "AAD",
-		price: 30.00,
-		colume: 12
-	},
-	{
-		symbol: "TDF",
-		price: 140.20,
-		colume: 1001
-	}
-]);
+var expensiveStock = getStockOver(
+	[
+		{
+			symbol: "XGC",
+			price: 240.00,
+			volume: 2342
+		},
+		{
+			symbol: "AAD",
+			price: 30.00,
+			volume: 12
+		},
+		{
+			symbol: "TDF",
+			price: 140.20,
+			volume: 1001
+		}
+	],
+	150.00
+);
 
-console.log(JSON.stringify(symbols));
+console.log(JSON.stringify(expensiveStock));
