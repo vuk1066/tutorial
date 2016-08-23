9cfb481f3b5b71bcc12f2bef0da70c1647ba81ee
Anett_Soos <anett.soos@city.ac.uk>
2016-08-23 15:19:34 +0100

    js concatAll method => flattens 2 or more dimensional arrays

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,39 @@
 // ES6 tutorial
-//forEach, map, filter
+//concatAll method => flatens 2 or more dimensional arrays
+/* use functions to flatten nested data structurs(arrays in arrays) so they can be
+further transformed */
 
-var stocks = [
-		{
-			symbol: "XGC",
-			price: 240.00,
-			volume: 2342
-		},
-		{
-			symbol: "AAD",
-			price: 30.00,
-			volume: 12
-		},
-		{
-			symbol: "TDF",
-			price: 140.20,
-			volume: 1001
-		}
+var exchanges = [
+		[
+			{ symbol: "XGC", price: 240.00, volume: 2342},
+			{ symbol: "AAD", price: 30.00, volume: 12}
+		],
+		[
+			{ symbol: "TFT", price: 140.00, volume: 1001},
+			{ symbol: "AAJ", price: 330.00, volume: 10000}
+		]
 ];
 
-var filteredStockSymbols =
-	stocks.
-		filter(function(stock) {
-			return stock.price >= 150.00
-		}).
-		map(function(stock) {
-			return stock.symbol;
-		}
-);
-
-filteredStockSymbols.forEach(function(symbol) {
-	console.log(symbol);
+exchanges.forEach(function(exchange) {
+	exchange.forEach(function(stock) {
+		console.log(JSON.stringify(stock));
+	});
 });
+
+Array.prototype.concatAll = function() {
+	var results = [];
+
+	this.forEach(function(subArray) {
+		subArray.forEach(function(item) {
+			results.push(item);
+		});
+	});
+	return results;
+};
+
+var stocks = exchanges.concatAll(); //same as above but using concatAll
+stocks.forEach(function(stock) {
+	console.log(JSON.stringify(stock));
+});
+
+
