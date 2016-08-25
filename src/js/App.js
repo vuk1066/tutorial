e579942004836b6ac54895e95f84fa1ad8658a40
Anett_Soos <anett.soos@city.ac.uk>
2016-08-25 11:28:17 +0100

    props
    passing values into the class components via props

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,15 @@
-'use strict'
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	render(){
-		return <div> Hello </div>
+		return <h1> {this.props.txt} </h1>
 	}
 }
+
+ReactDOM.render(
+	<App txt="this is the props text" />,
+	document.getElementById('app')
+);
 
 export default App
