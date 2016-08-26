873be320640816706c999f18a7f97e06cb50f85e
Anett_Soos <anett.soos@city.ac.uk>
2016-08-26 09:43:46 +0100

    react state basics
    creating an input box, txt appears in output field immediately

--- src/js/App.js
+++ src/js/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = { 
+			txt: 'this is the state',
+			cat: 0
+		}
+	}
+	update(e){
+		this.setState({txt: e.target.value})
+	}
 	render(){
-		let txt = this.props.txt
-		return <h1> {txt} </h1>
+		return (
+			<div>
+			<input type="text" 
+				onChange={this.update.bind(this)} />
+			<h1> {this.state.txt} </h1>
+			</div>
+		);
 	}
 }
-
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt: "this is the props text"
-}
-
-ReactDOM.render(
-	<App cat={5} />,
-	document.getElementById('app')
-);
 
 export default App
