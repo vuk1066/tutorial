5430a99dc0e86fc36bb13591b99a59a125270719
Anett_Soos <anett.soos@city.ac.uk>
2016-09-05 10:23:19 +0100

    React owner ownee relationship

--- src/js/App.js
+++ src/js/App.js
@@ -3,10 +3,8 @@ import React from 'react';
 class App extends React.Component {
 	constructor(){
 		super();
-		this.state = { 
-			txt: 'this is the state',
-			cat: 0
-		}
+		this.state = { txt: ''}
+		this.update = this.update.bind(this)
 	}
 	update(e){
 		this.setState({txt: e.target.value})
@@ -14,12 +12,22 @@ class App extends React.Component {
 	render(){
 		return (
 			<div>
-			<input type="text" 
-				onChange={this.update.bind(this)} />
-			<h1> {this.state.txt} </h1>
+				<Widget txt={this.state.txt} update={this.update} />
 			</div>
 		);
 	}
 }
 
+const Widget = (props) => {
+	return (
+			<div>
+				<input type="text" 
+					onChange={props.update} />
+				<h1> {props.txt} </h1>
+			</div>
+		);
+}
+
 export default App
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {digital_root} from './modules/math';
 ReactDOM.render(<App />, document.getElementById('app'))
