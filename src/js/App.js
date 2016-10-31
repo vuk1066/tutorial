be599ed700c4c13d9f2163391630f5def2f8a2c5
Anett_Soos <anett.soos@city.ac.uk>
2016-10-31 17:17:49 +0000

    practise state

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
+class App extends React.Component { 
+	constructor(){
+		super();
+		this.state = { txt: 'this is the state txt'}
+	}	
+  	render(){ 
+	    return ( 
+    	  <h1>{this.state.txt}</h1>
+      	); 
+  	} 
+} 
 
+export default App
