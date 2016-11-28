67aa5ac6c8960ee30bc8f4ab631eb2f48cd6d533
Anett_Soos <anett.soos@city.ac.uk>
2016-11-28 16:58:21 +0000

    this.props.children accessing innerHTML of other components

--- src/js/App.js
+++ src/js/App.js
@@ -1,47 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; 
 
 class App extends React.Component { 
-	constructor(){
-		super();
-		this.state = {
-			red: 0,
-			blue: 0,
-			green: 0
-		}
-		this.update = this.update.bind(this)
-	}
-	update(e){
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue).value,
-			green: ReactDOM.findDOMNode(this.refs.green).value
-		})
-	}
   	render(){ 
 	    return (
 	    	<div>
-	    		<Slider ref="green" update={this.update} />	
-	    		{this.state.green} <br />
-	    		<Slider ref="blue" update={this.update} />
-	    		{this.state.blue} <br />
-	    		<Slider ref="red" update={this.update} />  
-	    		{this.state.red}
+	    		<Button> I <Heart/> React </Button>
 	    	</div>
       	); 
   	} 
 }
 
-class Slider extends React.Component {
+class Button extends React.Component {
 	render(){
 		return (
-	    	<input type="range"
-	    		min="0"
-	    		max="255" 
-	    		onChange={this.props.update} />
-
+			<button>{this.props.children}</button>
 		);
 	}	
 }
+
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 export default App
