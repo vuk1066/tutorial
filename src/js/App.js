d84cb264dd94f9306f64b36c5b6de2e733b84bc7
Anett_Soos <anett.soos@city.ac.uk>
2016-11-30 17:19:15 +0000

    reusable component, printing from JSON

--- src/js/App.js
+++ src/js/App.js
@@ -1,74 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	constructor(){
 		super();
-		this.state = { 
-	      red: 0, 
-	      blue: 0, 
-	      green: 0 
-	    } 
-	    this.update = this.update.bind(this) 
-	  } 
-
-	  update(e){ 
-	    this.setState({ 
-	      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
-	    }) 
-	  } 
-	    render(){
-	    return ( 
-	        <div> 
-	          <NumInput 
-		          ref="red"
-		          min={0}
-		          max={255}
-		          step={1}
-		          val={+this.state.red}
-		          //type="number"
-		          label="Red"
-		          update={this.update} />   
-	        </div> 
-        );
-    }  
-}
-class NumInput extends React.Component { 
-  render(){
-  	let label = this.props.label !== '' ? 
-  	<label> {this.props.label} - {this.props.val}</label> : ''
-    return (
-    	<div>
-        <input ref = "inp"
-        	type = {this.props.type}
-        	min = {this.props.min}
-        	max = {this.props.max}
-        	step = {this.props.step}
-        	defaultValue = {this.props.val}
-        	onChange = {this.props.update} />
-        	{label}
-        </div>
-    );
-  }
+		this.state = {items: []}
+	}
+	componentWillMount(){
+		fetch('http://swapi.co/api/people/?format=json')
+			.then( response => response.json() )
+			.then( ({results: items}) => this.setState({items}))
+	}
+	render(){
+		let items = this.state.items
+		return (
+			<div>
+				{items.map(item => 
+					<Person key={item.name} /> )}
+			</div>
+		)
+	}
 }
 
-NumInput.propTypes = {
-	min: React.PropTypes.number,
-	max: React.PropTypes.number,
-	step: React.PropTypes.number,
-	val: React.PropTypes.number,
-	label: React.PropTypes.string,
-	update: React.PropTypes.func.isRequired,
-	type: React.PropTypes.oneOf(['number','range'])
-}
-
-NumInput.defaultProps = {
-	min: 0,
-	max: 0,
-	step: 1,
-	val: 0,
-	label: '',
-	type: 'range'
-}
+const Person = (props) => <h4>{props.person.name}</h4>
 
 export default App
