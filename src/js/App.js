c3072d1d5b36d2a208dbb2f39ef94a3af556a164
Anett_Soos <anett.soos@city.ac.uk>
2016-12-08 15:53:09 +0000

    Router - props/links

--- src/js/App.js
+++ src/js/App.js
@@ -1,39 +1,29 @@
 import React from 'react';
+import {Router, Route, Link, hashHistory} from 'react-router';
+
+const Home = (props) => <div><h1> Home </h1><Links /> {props.children} </div>;
+const About = (props) => <div><h1> About </h1> {props.children} </div>;
+const Contact = () => <div><h1> Contact </h1><Links /></div>;
+
+const Links = () =>
+	<nav>
+		<Link to="/"> Home </Link>
+		<Link to="/about"> About </Link>
+		<Link to="about/contact"> Contact </Link>
+	</nav>
 
 class App extends React.Component {
 	render(){
 		return (
-			<Buttons>
-				<button value="A">A</button>
-				<button value="B">B</button>
-				<button value="C">C</button>
-			</Buttons>
-		)
+			<Router history={ hashHistory }>
+				<Route path="/" component={Home}>
+					<Route path="about" component={About}>
+						<Route path="contact" component={Contact}></Route>
+					</Route>
+				</Route>
+			</Router>
+		);
 	}
 }
 
-class Buttons extends React.Component {
-	constructor(){
-		super();
-		this.state = {selected : 'None'}
-	}
-	selectItem(selected){
-		this.setState({selected})
-	}
-	render(){
-		let fn = child =>
-			React.cloneElement(child, {
-				onClick: this.selectItem.bind(this, child.props.value)
-			})
-
-		let items = React.Children.map(this.props.children, fn);
-		return (
-			<div>
-				<h2> You have Selected: {this.state.selected}</h2>
-				{items}
-			</div>
-			)
-	}
-}
-
-export default App
+export default App;
