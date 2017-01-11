028e0112e30da1e316406d336bd8f5b61646592d
Anett_Soos <anett.soos@city.ac.uk>
2017-01-11 16:32:38 +0000

    ReactRouter setting up <Links /> <nav>

--- src/js/App.js
+++ src/js/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import {Router, Route, Link, hashHistory} from 'react-router';
+import { Router, Route, Link, hashHistory } from 'react-router';
 
-const Home = (props) => <div><h1> Home </h1><Links /> {props.children} </div>;
-const About = (props) => <div><h1> About </h1> {props.children} </div>;
-const Contact = () => <div><h1> Contact </h1><Links /></div>;
+const Home = () => <div><h1>Home</h1><Links /></div>;
+const About = () => <div><h1>About</h1><Links /></div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
 
 const Links = () =>
-	<nav>
-		<Link to="/"> Home </Link>
-		<Link to="/about"> About </Link>
-		<Link to="about/contact"> Contact </Link>
-	</nav>
+    <nav>
+      <Link to="/">Home </Link>
+      <Link to="about">About </Link>
+      <Link to="contact">Contact </Link>
+    </nav>
 
-class App extends React.Component {
-	render(){
-		return (
-			<Router history={ hashHistory }>
-				<Route path="/" component={Home}>
-					<Route path="about" component={About}>
-						<Route path="contact" component={Contact}></Route>
-					</Route>
-				</Route>
-			</Router>
-		);
-	}
-}
+
+const App = () => {
+  return (
+    <Router history={ hashHistory }>
+      <Route path="/" component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+      <Route path="/contact" component={Contact}></Route>
+    </Router>
+  )
+};
 
 export default App;
