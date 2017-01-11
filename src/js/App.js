4c5e4417ae4110c2fd0d7221bedb1771f5540bd0
Anett_Soos <anett.soos@city.ac.uk>
2017-01-11 16:51:20 +0000

    ReactRouter Nested Router; active links
    accessing the children of parent routes
    link -change colour of active links

--- src/js/App.js
+++ src/js/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Router, Route, Link, hashHistory } from 'react-router';
 
-const Home = () => <div><h1>Home</h1><Links /></div>;
-const About = () => <div><h1>About</h1><Links /></div>;
-const Contact = () => <div><h1>Contact</h1><Links /></div>;
+const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
+const About = (props) => <div><h1>About</h1>{props.children}</div>;
+const Contact = () => <div><h1>Contact</h1></div>;
 
 const Links = () =>
     <nav>
-      <Link to="/">Home </Link>
-      <Link to="about">About </Link>
-      <Link to="contact">Contact </Link>
-    </nav>
+      <Link activeStyle={{color: 'green'}} to="/">Home </Link>
+      <Link activeStyle={{color: 'green'}} to="/about">About </Link>
+      <Link activeClassName="active" to="/about/contact">Contact </Link>
+    </nav> 
 
 
 const App = () => {
   return (
     <Router history={ hashHistory }>
-      <Route path="/" component={Home}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/contact" component={Contact}></Route>
+      <Route path="/" component={Home}>
+        <Route path="about" component={About}>
+          <Route path="contact" component={Contact}></Route>
+        </Route>
+      </Route>
     </Router>
   )
 };
