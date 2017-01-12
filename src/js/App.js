0a258a265470046396a94fa81eece5bd448d368a
Anett_Soos <anett.soos@city.ac.uk>
2017-01-12 13:32:17 +0000

    ReacrRouter - IndexRoute

--- src/js/App.js
+++ src/js/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
 
-const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
-const About = (props) => <div><h1>About</h1>{props.children}</div>;
+const Outer = (props) => <div><h1>Our Site</h1><Links />{props.children}</div>;
+const About = () => <div><h1>About</h1></div>;
 const Contact = () => <div><h1>Contact</h1></div>;
 
 const Links = () =>
     <nav>
-      <Link activeStyle={{color: 'green'}} to="/">Home </Link>
-      <Link activeStyle={{color: 'green'}} to="/about">About </Link>
-      <Link activeClassName="active" to="/about/contact">Contact </Link>
+      <Link to="/">Home </Link>
+      <Link to="/about">About </Link>
+      <Link to="/contact">Contact </Link>
     </nav> 
 
 
 const App = () => {
   return (
-    <Router history={ hashHistory }>
-      <Route path="/" component={Home}>
-        <Route path="about" component={About}>
-          <Route path="contact" component={Contact}></Route>
-        </Route>
+    <Router history={ browserHistory }>
+      <Route path="/" component={Outer}>
+        <IndexRoute component={About}></IndexRoute>
+        <Route path="contact" component={Contact}></Route>
       </Route>
     </Router>
   )
