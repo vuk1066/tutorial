97f1d6c525061e7000d717f186f009cb9b584c4f
Anett <soosanett@gmail.com>
2017-01-15 20:32:31 +0000

    created some empty js files

--- src/js/components/profile.js
+++ src/js/components/profile.js
@@ -2,15 +2,27 @@ import React from 'react';
 import {Router, Route, Link} from 'react-router';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      notes:[],
+      bio:{},
+      repos: []
+    }
+  }
   render() {  
     return (
       <div className="row">
           <div className="col-md-4">
+         // user profile component --> {this.props.params.username}
+          //<DisplayRepos ChildRepos={this.state.repos} />
+          <p>User Profile Componetn</p>
            </div>
           <div className="col-md-4">
+          <p>Repos</p>
           </div>
           <div className="col-md-4">
-          some text
+          <p>Notes</p>
           </div>
       </div>
     );
--- src/js/components/search.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Search = () => (
-		<h2 className="text-center">TEST</h2>
-	); 
-
-export default Search;
