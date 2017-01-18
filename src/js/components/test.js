e98a4a855999c47d693d9bab092cedb956445ad1
Anett_Soos <anett.soos@city.ac.uk>
2017-01-18 15:37:38 +0000

    React Synthetic Event System
    which normalizes all the events we might want to use across various browsers.

--- src/js/components/test.js
+++ src/js/components/test.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
 class StateTest extends React.Component {
+	constructor(){
+		super();
+		this.state = {currentEvent: '---'}
+		this.update = this.update.bind(this)
+	}
+
+	update(e){
+		this.setState({currentEvent: e.type})
+	}
+
   	render(){
 	  	return (
 	      <div>
-	 		<Title />
+	 		<textarea 
+	 			onKeyPress={this.update}
+	 			onCopy={this.update}
+		        onCut={this.update}
+		        onPaste={this.update}
+		        onFocus={this.update}
+		        onBlur={this.update}
+		        onDoubleClick={this.update}
+		        onTouchStart={this.update}
+		        onTouchMove={this.update}
+		        onTouchEnd={this.update}
+	 			cols='30' rows='10' />
+	 		<h1>{this.state.currentEvent}</h1>
 	      </div>
      	);
   	}
-}
-
-const Title = (props) => <h1>Title: {props.txt} </h1>
-
-Title.propTypes = {
-	txt(props, propName, component){
-		if(!(propName in props)){
-			return new Error(`missing ${propName}`)
-		}
-	}
 }
 
 export default StateTest;
