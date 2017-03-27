cd9effba4388d0f709648381c81a862a70822684
Anett_Soos <anett.soos@city.ac.uk>
2017-03-27 15:53:27 +0100

    setting up react app - routers

---
+++ rr4/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+const Home = (props) => {
+  console.log(props)
+  return <h1>Home</h1>
+}
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route strict path="/about" render={() => <h1>About</h1>} />
+      <Route path="/about" children={({match}) => match && <h2>Child router always runs</h2>} />
+    </div>
+  </Router>
+);
+
+export default App
