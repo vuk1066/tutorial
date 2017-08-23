b736f7a4539c788e8db18b3f9a3d3bfc4ba9de89
Anett_Soos <anett.soos@city.ac.uk>
2017-08-23 15:05:11 +0100

    apollo installed

--- coolio-app/src/App.js
+++ coolio-app/src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ChannelsList = () =>
+    (<ul className="Item-list">
+      <li>Channel 1</li>
+      <li>Channel 2</li>
+    </ul>);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Apollo</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChannelsList />
       </div>
     );
   }
